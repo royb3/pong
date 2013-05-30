c3324ac51bd4f3773db8a9edd749ed8fd23a0a1f
Vincent Molenaar <vincent_molenaar7@hotmail.com>
2013-05-30 12:23:22 +0200

    add socket.io

---
+++ Wpf_App/wpf_Pong/wpf_Pong/Socket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocketIOClient;
+
+namespace wpf_Pong
+{
+    class Socket
+    {
+        public Client client;
+        public void StartConnection(string Ip)
+        {
+            //Console.WriteLine("Starting TestSocketIOClient Example...");
+
+            client = new Client(Ip);
+
+            // register for 'connect' event with io server
+            client.On("succesfull", (data) =>
+            {
+                //MessageBox.Show("asd");
+            });
+
+            // register for 'update' events - message is a json 'Part' object
+            client.On("playerJoined", (data) =>
+            {
+                //MessageBox.Show(data.RawMessage);
+                //SimpleJson.SimpleJson.DeserializeObject<Player>(data.RawMessage);
+            });
+
+            // make the socket.io connection
+
+            client.Connect();
+        }
+    }
+}
